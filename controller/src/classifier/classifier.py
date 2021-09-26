73b3d3508779aef259aaf118844d6bafb51b5930
Eyuel786 <eyuelworku786@gmail.com>
2021-09-26 08:07:47 +0300

    added cascade classifier and edited the code

--- controller/src/classifier/classifier.py
+++ controller/src/classifier/classifier.py
@@ -3,18 +3,16 @@ import cv2
 
 class Classifier:
     def __init__(self, classifier_path=None):
-        self.classifier_path = 'cascade.xml' if classifier_path is None else classifier_path
+        self.classifier_path = 'cars.xml' if classifier_path is None else classifier_path
         self.image_resized = None
         self.cars = None
 
     def count_cars(self, img_path) -> int:
         car_classifier = cv2.CascadeClassifier(self.classifier_path)
         image = cv2.imread(img_path)
-        print(img_path)
-        print(image)
-        self.image_resized = cv2.resize(image, (64, 64))
+        self.image_resized = cv2.resize(image, (320, 240))
         gray = cv2.cvtColor(self.image_resized, cv2.COLOR_BGR2GRAY)
-        self.cars = car_classifier.detectMultiScale(gray)
+        self.cars = car_classifier.detectMultiScale(gray, 1.1, 2)
         return len(self.cars)
 
     def draw_on_detected_cars(self):
@@ -22,9 +20,7 @@ class Classifier:
             print('please use count_cars() first')
         else:
             for (x, y, w, h) in self.cars:
-                cv2.rectangle(self.image_resized, (x, y), (x + w, y + h), (255, 0, 0), 3)
+                cv2.rectangle(self.image_resized, (x, y), (x + w, y + h), (255, 0, 0), 2)
             cv2.imshow('img', self.image_resized)
             cv2.waitKey()
             cv2.destroyAllWindows()
-
-
