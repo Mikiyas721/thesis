4dd961ba92f071e258e11c2e05069a011a2a522f
Mikiyas721 <Mikiyas721@gmail.com>
2021-09-29 13:45:04 -0600

    Added Final Doc

--- controller/src/classifier/classifier.py
+++ controller/src/classifier/classifier.py
@@ -3,14 +3,14 @@ import cv2
 
 class Classifier:
     def __init__(self, classifier_path=None):
-        self.classifier_path = 'cars.xml' if classifier_path is None else classifier_path
+        self.classifier_path = 'cascade.xml' if classifier_path is None else classifier_path
         self.image_resized = None
         self.cars = None
 
     def count_cars(self, img_path) -> int:
         car_classifier = cv2.CascadeClassifier(self.classifier_path)
         image = cv2.imread(img_path)
-        self.image_resized = cv2.resize(image, (320, 240))
+        self.image_resized = cv2.resize(image, (320, 320))
         gray = cv2.cvtColor(self.image_resized, cv2.COLOR_BGR2GRAY)
         self.cars = car_classifier.detectMultiScale(gray, 1.1, 2)
         return len(self.cars)
@@ -24,3 +24,8 @@ class Classifier:
             cv2.imshow('img', self.image_resized)
             cv2.waitKey()
             cv2.destroyAllWindows()
+
+
+# classifier = Classifier()
+# classifier.count_cars('E:\\Files\\Code\\Thesis\\controller\\src\\classifier\\test_images\\0.jpg')
+# classifier.draw_on_detected_cars()
--- controller/src/main.py
+++ controller/src/main.py
@@ -1,7 +1,7 @@
 import time
 from datetime import datetime
 
-import src.hw.hardware_manipulator as hw
+# import src.hw.hardware_manipulator as hw
 from src.classifier.classifier import Classifier
 from src.http.loopback_client import LoopbackClient
 from src.scheduler.assigned_state import AssignedState
@@ -36,14 +36,14 @@ def main():
     classifier = Classifier()
 
     density_state = DensityState(
-        car_density=[classifier.count_cars('./test_images/0.jpg'),
-                     classifier.count_cars('./test_images/1.jpg'),
-                     classifier.count_cars('./test_images/2.jpg'),
-                     classifier.count_cars('./test_images/3.jpg'),
-                     classifier.count_cars('./test_images/4.jpg'),
-                     classifier.count_cars('./test_images/5.jpg'),
-                     classifier.count_cars('./test_images/6.jpg'),
-                     classifier.count_cars('./test_images/7.jpg')],
+        car_density=[classifier.count_cars('E:\\Files\\Code\\Thesis\\controller\\src\\classifier\\test_images\\0.jpg'),
+                     classifier.count_cars('E:\\Files\\Code\\Thesis\\controller\\src\\classifier\\test_images\\1.jpg'),
+                     classifier.count_cars('E:\\Files\\Code\\Thesis\\controller\\src\\classifier\\test_images\\2.jpg'),
+                     classifier.count_cars('E:\\Files\\Code\\Thesis\\controller\\src\\classifier\\test_images\\3.jpg'),
+                     classifier.count_cars('E:\\Files\\Code\\Thesis\\controller\\src\\classifier\\test_images\\4.jpg'),
+                     classifier.count_cars('E:\\Files\\Code\\Thesis\\controller\\src\\classifier\\test_images\\1.jpg'),
+                     classifier.count_cars('E:\\Files\\Code\\Thesis\\controller\\src\\classifier\\test_images\\2.jpg'),
+                     classifier.count_cars('E:\\Files\\Code\\Thesis\\controller\\src\\classifier\\test_images\\3.jpg')],
     )
     scheduler = Scheduler(AssignedState(), density_state)
     assigned_state = scheduler.calculate()
@@ -63,11 +63,11 @@ def main():
             now = datetime.now()
 
         # controlling Lights
-        hw.turn_greens_on(assigned_state.get_on_led_s())
-        sleep(assigned_state.allocated_time)
+        # hw.turn_greens_on(assigned_state.get_on_led_s())
+        # sleep(assigned_state.allocated_time)
 
-        hw.turn_yellows_on(assigned_state.get_on_led_s())
-        sleep(3)
+        # hw.turn_yellows_on(assigned_state.get_on_led_s())
+        # sleep(3)
 
         # In actual application order cameras to take pictures and stores them in test_images folder labeled 0-7
 
@@ -77,10 +77,7 @@ def main():
                          classifier.count_cars('./test_images/1.jpg'),
                          classifier.count_cars('./test_images/2.jpg'),
                          classifier.count_cars('./test_images/3.jpg'),
-                         classifier.count_cars('./test_images/4.jpg'),
-                         classifier.count_cars('./test_images/5.jpg'),
-                         classifier.count_cars('./test_images/6.jpg'),
-                         classifier.count_cars('./test_images/7.jpg')],
+                         classifier.count_cars('./test_images/4.jpg')],
         )
         assigned_state = scheduler.calculate()
         scheduler = Scheduler(assigned_state, density_state)
